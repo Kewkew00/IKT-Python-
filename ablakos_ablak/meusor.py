63b02dc7ffb91267783b645dd488499906bb86ae
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2022-04-12 08:46:17 +0200

    henger hozzáadása

--- ablakos_ablak/meusor.py
+++ ablakos_ablak/meusor.py
@@ -1,6 +1,7 @@
 from cProfile import label
 from tkinter import *
 from turtle import left 
+import math
 
 
 #nevjegy ablak
@@ -32,6 +33,7 @@ def felszin():
         felszin = 2*(a*b+a*c+b*c)
 
         if a <= 0 or b <= 0 or c <= 0:
+            mezo4.delete(0, END)
             mezo4.insert(0,str()+ 'Negatív számmal nem lehet számolni')
         else:
             mezo4.delete(0, END)
@@ -76,6 +78,7 @@ def terfogat():
         terfogat = a*b*c
 
         if a <= 0 or b <= 0 or c <= 0:
+            mezo4.delete(0, END)
             mezo4.insert(0,str()+ 'Negatív számmal nem lehet számolni')
         else:
             mezo4.delete(0, END)
@@ -106,6 +109,89 @@ def terfogat():
     ablak3.mainloop()
 #terfogat ablak vége
 
+#henger terfogat ablak kezdete
+def hterfogat():
+    s = ''
+    def hszamit():
+
+        if not s:
+            mezo4.delete(0, END)
+            mezo4.insert(0, str()+ ' A számításhoz számadat kell')
+
+        r = eval(mezo1.get())
+        m = eval(mezo2.get())
+        terfogat = math.pi * r * r * m
+
+        if r <= 0 or m <= 0:
+            mezo4.delete(0, END)
+            mezo4.insert(0,str()+ 'Negatív számmal nem lehet számolni')
+        else:
+            mezo4.delete(0, END)
+            mezo4.insert(0, str(felszin))
+
+
+    ablak4 = Toplevel(foablak)
+    ablak4.title('A henger térfogata')
+    ablak4.minsize(width = 300, height = 100)
+    szoveg1 = Label(ablak4, text = 'r')
+    szoveg2 = Label(ablak4, text = 'm')
+    szoveg4 = Label(ablak4, text = 'Eredmény: ')
+    gomb1 = Button(ablak4,  text = 'Számítás', command = hszamit)
+    mezo1 = Entry(ablak4)
+    mezo2 = Entry(ablak4)
+    mezo4 = Entry(ablak4)
+    szoveg1.grid(row = 1)
+    szoveg2.grid(row = 2)
+    szoveg4.grid(row = 5)
+    gomb1.grid(row = 4, column = 2, sticky = W)
+    mezo1.grid(row = 1, column = 2, sticky = W)
+    mezo2.grid(row = 2, column = 2, sticky = W)
+    mezo4.grid(row = 5, column = 2, sticky = W)
+    ablak4.mainloop()
+#henger terfogat ablavége
+
+#henger felsznin kezdete
+def hfelszin():
+    
+    s = ''
+
+    def hszamit():
+
+        if not s:
+            mezo4.delete(0, END)
+            mezo4.insert(0, str()+ 'A számításhoz számadat kell')
+
+        r = eval(mezo1.get())
+        m = eval(mezo2.get())
+        felszin = 2*math.pi*r(r+m)
+
+        if r <= 0 or m <= 0:
+            mezo4.delete(0, END)
+            mezo4.insert(0,str()+ 'Negatív számmal nem lehet számolni')
+        else:
+            mezo4.delete(0, END)
+            mezo4.insert(0, str(felszin))
+
+    ablak5 =Toplevel(foablak)
+    ablak5.title('A henger felszíne')
+    ablak5.minsize(width = 300, height = 100)
+    szoveg1 = Label(ablak5, text = 'r:')
+    szoveg2 = Label(ablak5, text = 'm:')
+    szoveg4 = Label(ablak5, text = 'Eredmény: ')
+    gomb1 = Button(ablak5,  text = 'Számítás', command = hszamit)
+    mezo1 = Entry(ablak5)
+    mezo2 = Entry(ablak5)
+    mezo4 = Entry(ablak5)
+    szoveg1.grid(row = 1)
+    szoveg2.grid(row = 2)
+    szoveg4.grid(row = 5)
+    gomb1.grid(row = 4, column = 2, sticky = W)
+    mezo1.grid(row = 1, column = 2, sticky = W)
+    mezo2.grid(row = 2, column = 2, sticky = W)
+    mezo4.grid(row = 5, column = 2, sticky = W)
+    ablak5.mainloop()
+#henger felszin vege
+
 #foablak
 foablak = Tk()
 foablak.title('A téglatest adatai')
@@ -114,18 +200,31 @@ foablak.minsize(width = 300, height = 100)
 menusor = Frame(foablak)
 menusor.pack(side = TOP, fill = X)
 
+#fájl menü
 menu1 = Menubutton(menusor, text = 'Fájl')
 menu1.pack(side = LEFT)
 fajl = Menu(menu1)
 fajl.add_command(label ='Névjegy', command = nevjegy, )
 fajl.add_command(label = 'Kilépés', command = foablak.destroy)
 menu1.config(menu = fajl)
+#fájl menü
 
+#téglatest menü
 menu2 = Menubutton(menusor, text = 'Téglatest')
 menu2.pack(side = LEFT)
 teglatest = Menu(menu2)
 teglatest.add_command(label = 'Felszin', command = felszin)
 teglatest.add_command(label = 'Térfogat', command = terfogat)
 menu2.config(menu = teglatest)
+#téglatest menü
+
+#hengermenü 
+menu3 = Menubutton(menusor, text = 'Henger')
+menu3.pack(side = LEFT)
+henger = Menu(menu3)
+henger.add_command(label = 'Felszin', command = hfelszin)
+henger.add_command(label = 'Térfogat', command = hterfogat)
+menu3.config(menu = henger)
+#hengermenü
 
 foablak.mainloop()
